0f94e8e415b2091f693a8518983d2f3f0ec870b1
Armanzdi <armanzahedi24@gmail.com>
2021-01-28 16:03:51 +0330

    fixed popup

---
+++ SpadCompanyPanel.Infrastructure/Repositories/InvoicesRepository.cs
@@ -0,0 +1,33 @@
+﻿using SpadCompanyPanel.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SpadCompanyPanel.Infrastructure.Repositories
+{
+    public class InvoicesRepository : BaseRepository<Invoice, MyDbContext>
+    {
+        private readonly MyDbContext _context;
+        private readonly LogsRepository _logger;
+        public InvoicesRepository(MyDbContext context, LogsRepository logger) : base(context, logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        public List<Invoice> GetInvoices()
+        {
+            return _context.Invoices.Include(i => i.Customer.User).Include(i=>i.Plan.RealState).OrderByDescending(i=>i.RegisteredDate).ToList();
+        }
+        public Invoice GetInvoice(int invoiceId)
+        {
+            return _context.Invoices.Include(i => i.Customer.User).FirstOrDefault(i => i.Id == invoiceId);
+        }
+        public List<Invoice> GetCustomerInvoices(int customerId)
+        {
+            return _context.Invoices.Where(i => i.IsDeleted == false && i.CustomerId == customerId).ToList();
+        }
+    }
+}
---
+++ SpadCompanyPanel.Web/Areas/Admin/Controllers/InvoicesController.cs
@@ -0,0 +1,74 @@
+﻿using SpadCompanyPanel.Core.Models;
+using SpadCompanyPanel.Core.Utility;
+using SpadCompanyPanel.Infrastructure.Repositories;
+using SpadCompanyPanel.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpadCompanyPanel.Web.Areas.Admin.Controllers
+{
+    [Authorize]
+    public class InvoicesController : Controller
+    {
+        private readonly InvoicesRepository _repo;
+        private readonly GeoDivisionsRepository _GeoRepo;
+
+        public InvoicesController(InvoicesRepository repo)
+        {
+            _repo = repo;
+        }
+        // GET: Admin/Invoices
+        public ActionResult Index()
+        {
+            var invoices = _repo.GetInvoices();
+            var vm = new List<InvoiceTableViewModel>();
+            foreach (var invoice in invoices)
+            {
+                vm.Add(new InvoiceTableViewModel(invoice));
+            }
+            return View(vm);
+        }
+        public bool ConfirmPayment(int id)
+        {
+            try
+            {
+                var invoice = _repo.Get(id);
+                invoice.PaymentStatus = (int)PaymentStatus.Confirmed;
+                _repo.Update(invoice);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Invoice invoice = _repo.Get(id.Value);
+            if (invoice == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PersianDate = new PersianDateTime(invoice.RegisteredDate).ToString();
+            return PartialView(invoice);
+        }
+
+        // POST: Admin/Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var invoice = _repo.Get(id);
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
